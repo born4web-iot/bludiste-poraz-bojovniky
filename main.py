4c02b079ce6db912f3908802d8ee487580ae5e80
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-11-19 20:50:07 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,91 @@
-tiles.set_current_tilemap(tilemap("""
-    level1
-"""))
+@namespace
+class SpriteKind:
+    FinalPrize = SpriteKind.create()
+def createXBounceEnemy(X: number, Y: number, XSpeed: number):
+    global XEnemy
+    XEnemy = sprites.create(img("""
+            ..............cfff..............
+                    ............ccddbf..............
+                    ...........cbddbff.........ccc..
+                    ..........fccbbcf.........cbbc..
+                    ...fffffffccccccff.......cdbc...
+                    .ffcbbbbbbbbbbbbbcfff....cdbf...
+                    fcbbbbbbbbbcbbbbbbcccff.cdbf....
+                    fbcbbbbffbbbcbcbbbcccccffdcf....
+                    fbb1111ffbbbcbcbbbccccccbbcf....
+                    .fb11111111bbcbbbccccccccbbcf...
+                    ..fccc33cb11bbbbcccccccfffbbf...
+                    ...fc131c111bbbcccccbdbc..fbbf..
+                    ....f33c111cbbccdddddbc....fff..
+                    .....ff1111fdbbccddbcc..........
+                    .......cccccfdbbbfcc............
+                    .............fffff..............
+        """),
+        SpriteKind.enemy)
+    XEnemy.set_position(X, Y)
+    XEnemy.set_bounce_on_wall(True)
+    XEnemy.set_velocity(XSpeed, 0)
+def showXYPosition(movingSprite: Sprite):
+    scene.camera_follow_sprite(movingSprite)
+    print("X: " + str(movingSprite) + " Y: " + str(movingSprite))
+def createTreasure():
+    global finalReward
+    finalReward = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . b d b . . . . . . 
+                    . . . . . . . b d b c . . . . . 
+                    . . . . b b c 5 5 5 c b b . . . 
+                    . . . . b 5 5 5 1 5 5 5 b . . . 
+                    . . . c c 5 5 5 1 5 5 5 c c . . 
+                    . . b b 5 5 5 1 1 1 5 5 5 b b . 
+                    . . d d 5 1 1 1 1 1 1 1 5 d d . 
+                    . . b b 5 5 5 1 1 1 5 5 5 b b . 
+                    . . . c c 5 5 5 1 5 5 5 c c . . 
+                    . . . . b 5 5 5 1 5 5 5 b . . . 
+                    . . . . b b c 5 5 5 c b b . . . 
+                    . . . . . . c b d b c . . . . . 
+                    . . . . . . . b d b . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.FinalPrize)
+    finalReward.set_position(335, 277)
+def createYBounceEnemy(X2: number, Y2: number, YSpeed: number):
+    global XEnemy
+    XEnemy = sprites.create(img("""
+            ..............cfff..............
+                    ............ccddbf..............
+                    ...........cbddbff.........ccc..
+                    ..........fccbbcf.........cbbc..
+                    ...fffffffccccccff.......cdbc...
+                    .ffcbbbbbbbbbbbbbcfff....cdbf...
+                    fcbbbbbbbbbcbbbbbbcccff.cdbf....
+                    fbcbbbbffbbbcbcbbbcccccffdcf....
+                    fbb1111ffbbbcbcbbbccccccbbcf....
+                    .fb11111111bbcbbbccccccccbbcf...
+                    ..fccc33cb11bbbbcccccccfffbbf...
+                    ...fc131c111bbbcccccbdbc..fbbf..
+                    ....f33c111cbbccdddddbc....fff..
+                    .....ff1111fdbbccddbcc..........
+                    .......cccccfdbbbfcc............
+                    .............fffff..............
+        """),
+        SpriteKind.enemy)
+    XEnemy.set_position(X2, Y2)
+    XEnemy.set_bounce_on_wall(True)
+    XEnemy.set_velocity(0, YSpeed)
+
+def on_on_overlap(sprite, otherSprite):
+    game.game_over(True)
+sprites.on_overlap(SpriteKind.player, SpriteKind.FinalPrize, on_on_overlap)
+
+def on_on_overlap2(sprite2, otherSprite2):
+    game.game_over(False)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+finalReward: Sprite = None
+XEnemy: Sprite = None
+tiles.set_current_tilemap(tilemap("""level1"""))
 Jedi = sprites.create(img("""
         ........................
             .......fff..............
@@ -28,5 +113,17 @@ Jedi = sprites.create(img("""
             ........................
     """),
     SpriteKind.player)
-controller.move_sprite(Jedi)
+Jedi.set_position(30, 35)
+controller.move_sprite(Jedi, 100, 100)
 scene.camera_follow_sprite(Jedi)
+movingSprite2 = Jedi
+createXBounceEnemy(50, 80, 50)
+createXBounceEnemy(50, 80, 60)
+createXBounceEnemy(414, 370, 60)
+createXBounceEnemy(380, 229, 70)
+createYBounceEnemy(289, 352, 70)
+createTreasure()
+
+def on_update_interval():
+    pass
+game.on_update_interval(1000, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const FinalPrize = SpriteKind.create()
+}
 function createXBounceEnemy (X: number, Y: number, XSpeed: number) {
     XEnemy = sprites.create(img`
         ..............cfff..............
@@ -21,15 +24,62 @@ function createXBounceEnemy (X: number, Y: number, XSpeed: number) {
     XEnemy.setBounceOnWall(true)
     XEnemy.setVelocity(XSpeed, 0)
 }
-function showXYPosition (mySprite: Sprite) {
-    scene.cameraFollowSprite(mySprite)
-    console.log("X: " + mySprite.x + " Y: " + mySprite.y)
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite2, otherSprite2) {
     game.gameOver(false)
 })
+function showXYPosition (movingSprite: Sprite) {
+    scene.cameraFollowSprite(movingSprite)
+    console.log("X: " + ("" + movingSprite) + " Y: " + ("" + movingSprite))
+}
+function createTreasure () {
+    finalReward = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.FinalPrize)
+    finalReward.setPosition(335, 277)
+}
+function createYBounceEnemy (X2: number, Y2: number, YSpeed: number) {
+    XEnemy = sprites.create(img`
+        ..............cfff..............
+        ............ccddbf..............
+        ...........cbddbff.........ccc..
+        ..........fccbbcf.........cbbc..
+        ...fffffffccccccff.......cdbc...
+        .ffcbbbbbbbbbbbbbcfff....cdbf...
+        fcbbbbbbbbbcbbbbbbcccff.cdbf....
+        fbcbbbbffbbbcbcbbbcccccffdcf....
+        fbb1111ffbbbcbcbbbccccccbbcf....
+        .fb11111111bbcbbbccccccccbbcf...
+        ..fccc33cb11bbbbcccccccfffbbf...
+        ...fc131c111bbbcccccbdbc..fbbf..
+        ....f33c111cbbccdddddbc....fff..
+        .....ff1111fdbbccddbcc..........
+        .......cccccfdbbbfcc............
+        .............fffff..............
+        `, SpriteKind.Enemy)
+    XEnemy.setPosition(X2, Y2)
+    XEnemy.setBounceOnWall(true)
+    XEnemy.setVelocity(0, YSpeed)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.FinalPrize, function (sprite, otherSprite) {
+    game.gameOver(true)
+})
+let finalReward: Sprite = null
 let XEnemy: Sprite = null
-let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 let Jedi = sprites.create(img`
     ........................
@@ -60,9 +110,13 @@ let Jedi = sprites.create(img`
 Jedi.setPosition(30, 35)
 controller.moveSprite(Jedi, 100, 100)
 scene.cameraFollowSprite(Jedi)
-mySprite = Jedi
+let movingSprite2 = Jedi
 createXBounceEnemy(50, 80, 50)
-createXBounceEnemy(50, 80, -80)
+createXBounceEnemy(50, 80, 60)
+createXBounceEnemy(414, 370, 60)
+createXBounceEnemy(380, 229, 70)
+createYBounceEnemy(289, 352, 70)
+createTreasure()
 game.onUpdateInterval(1000, function () {
 	
 })
